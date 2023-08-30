854fc94a20b0cec0fa81726308d65a0f7866e4a3
Park Dongkyu <qkrehdrb0813@gmail.com>
2023-08-30 16:30:02 +0900

    user controller 수정

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const userRouter = express.Router();
-const userController = require("./userController1");
+const userController = require("..controllers/userController");
 
-
-userRouter.route('/')
-    .get(userController.getUsers);
-userRouter.route('/detail/:email')
-    .get(userController.getUser);
-userRouter.route('/:emails')
-    .put(userController.updateUser);
-userRouter.route('/check/:code')
-    .get(userController.checkCouple);
-userRouter.route('/deletecouple/:connectCode')
-    .put(userController.deleteCouple);
+userRouter.route("/").get(userController.getUsers);
+userRouter.route("/detail/:email").get(userController.getUser);
+userRouter.route("/:emails").put(userController.updateUser);
+userRouter.route("/check/:code").get(userController.checkCouple);
+userRouter.route("/deletecouple/:connectCode").put(userController.deleteCouple);
 
 module.exports = userRouter;
